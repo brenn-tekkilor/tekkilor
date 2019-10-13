6b5dfa514c9530be0d681073fe48ce506c3e0ac4
Brenn Ayr Tekkilor <brenn.tekkilor@gmail.com>
2019-10-12 18:00:25 -0700

    removed bridge

--- com/obj/Debug/netcoreapp3.0/com.AssemblyInfo.cs
+++ com/obj/Debug/netcoreapp3.0/com.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Com")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Com")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Com")]
+[assembly: System.Reflection.AssemblyProductAttribute("com")]
+[assembly: System.Reflection.AssemblyTitleAttribute("com")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
 
